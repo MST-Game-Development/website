503865720b643e5abc7ce19ee4b29e70acbb61cf
agiveygives <justincase25xxv@gmail.com>
2019-11-02 00:42:12 -0500

    Routes and Read ME updates

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,28 +1,42 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+
+// Icons
+import HomeIcon from '@material-ui/icons/Home';
+import DateRangeIcon from '@material-ui/icons/DateRange';
+
+// Pages
 import HomePage from './pages/HomePage';
 import SchedulePage from './pages/SchedulePage';
-
-const Routes = () => (
-  <content>
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/schedule" component={SchedulePage} />
-    </Switch>
-  </content>
-);
 
 export const routeConfig = [
   {
     display: 'Home',
     key: 'index',
-    route: '/'
+    route: '/',
+    icon: <HomeIcon />
   },
   {
     display: 'Schedule',
     key: 'schedule',
-    route: '/schedule'
+    route: '/schedule',
+    icon: <DateRangeIcon />
   }
 ];
+
+if (window.location.hostname === 'mst-game-development.github.io') {
+  routeConfig.forEach(config => {
+    config.route = `/website${config.route}`;
+  });
+}
+
+const Routes = () => (
+  <content>
+    <Switch>
+      <Route exact path={routeConfig[0].route} component={HomePage} />
+      <Route exact path={routeConfig[1].route} component={SchedulePage} />
+    </Switch>
+  </content>
+);
 
 export default Routes;
